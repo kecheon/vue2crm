8dbba6437ac898f512b383bb80e7d80191471f3f
kecheon <kecheon@gmail.com>
2020-05-10 15:53:15 +0900

    refactor view

--- src/App.vue
+++ src/App.vue
@@ -52,36 +52,19 @@
       <v-toolbar app>
         <v-toolbar-side-icon @click.native.stop="drawer = !drawer" light></v-toolbar-side-icon>
         <v-spacer></v-spacer>
-        <div class="text-xs-center pr-3">
-          <v-badge left="">
-            <span slot="badge">6</span>
-            <v-icon large color="grey lighten-1">shopping_cart</v-icon>
-          </v-badge>
-
-          <v-badge color="red">
-            <span slot="badge">!</span>
-            <v-icon large color="grey">mail</v-icon>
-          </v-badge>
-        </div>
-        <v-btn light flat  href="https://github.com/harryho/vue2crm" target="_blank">
-                <svg height="30" class="octicon octicon-mark-github" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true">
-                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-                  </svg>
-            <span style="margin-left:0.4rem;">GitHub</span>
-          </v-btn>
       </v-toolbar>
       <v-content>
         <v-container fluid >
-          <!-- <v-layout>
-            <v-flex row=""> -->
+          <v-layout>
+            <v-flex row="">
               <router-view></router-view>
-            <!-- </v-flex>
-          </v-layout> -->
+            </v-flex>
+          </v-layout>
         </v-container>
       </v-content>
       <!-- <canvas id="canvas"></canvas> -->
       <v-footer :inset="true" style="justify-content:center; text-align: center" app>
-        <span>&copy; Vue-CRM 2018</span>
+        <span>&copy; 율담관세사무소 2020</span>
       </v-footer>
     </template>
   </v-app>
@@ -103,12 +86,12 @@
         drawer: window.innerWidth > 960,
         fixed: false,
         items: [
-          {
-            icon: "dashboard",
-            title: "Dashboard",
-            vertical: "Dashboard",
-            link: "dashboard"
-          },
+          // {
+          //   icon: "dashboard",
+          //   title: "Dashboard",
+          //   vertical: "Dashboard",
+          //   link: "dashboard"
+          // },
           {
             icon: "shopping_cart",
             title: "Invoices",
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,11 +2,8 @@
 
   <v-flex xs12>
     <p class="text-lg-center display-1">About</p>
-
-    <p class="text-xs-center title" ><span class="cyan--text darken-3">Vue CRM App Demo</span></p>
-
-    <p class="text-xs-center subheader">This demo app is not a real application. There is no fake API as back-end service behind the scene. Its advanced search doesn't work properly. Any data update (create or update record) will not be stored after hard refresh or logout. The main purpose of this demo is just a proof of concept.
-    </p>
+    <p class="text-xs-center title" ><span class="cyan--text darken-3">율담관세사무소 CRM</span></p>
+    <p class="text-xs-center subheader">이 앱은 율담관세사무소의 관세사 업무를 지원하기 위한 것입니다.</p>
   </v-flex>
 
 </template>
